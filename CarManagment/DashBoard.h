79fd53f19e8ecfa411fde2128bb2e9778af310d6
Schatten900 <carloscaua.silva9@gmail.com>
2024-03-13 10:56:48 -0300

    Adicionar arquivos de projeto.

---
+++ CarManagment/DashBoard.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace CarManagment {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Sumário para DashBoard
+	/// </summary>
+	public ref class DashBoard : public System::Windows::Forms::Form
+	{
+	public:
+		DashBoard(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Adicione o código do construtor aqui
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		~DashBoard()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// DashBoard
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(876, 431);
+			this->Name = L"DashBoard";
+			this->Text = L"DashBoard";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
